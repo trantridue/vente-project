45109cccebeb3fc8a4c6da6be80fdd6fb8ece3eb
Tran TRI DUE <dtrantri@pentalog.fr>
2016-10-08 22:27:47 +0700

    clean code : update pom

--- vente.ws/src/main/java/com/nordnet/vente/ws/controller/UserController.java
+++ vente.ws/src/main/java/com/nordnet/vente/ws/controller/UserController.java
@@ -53,22 +53,26 @@ public class UserController extends WadlController {
 
 	@RequestMapping(value = "all", method = GET, produces = APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public List<User> getAllUser() throws ConverterException {
-		// List<com.nordnet.vente.ws.entities.User> users = new ArrayList<com.nordnet.vente.ws.entities.User>();
-		// for (User user : userService.getAllUser()) {
-		// users.add(converter.convert(user, com.nordnet.vente.ws.entities.User.class));
-		// }
-		// return users;
-		return userService.getAllUser();
+	public List<com.nordnet.vente.ws.entities.User> getAllUser()
+			throws ConverterException {
+		List<com.nordnet.vente.ws.entities.User> users = new ArrayList<com.nordnet.vente.ws.entities.User>();
+		for (User user : userService.getAllUser()) {
+			users.add(converter.convert(user,
+					com.nordnet.vente.ws.entities.User.class));
+		}
+		return users;
+//		 return userService.getAllUser();
 	}
 
 	@RequestMapping(value = "/get/byname", method = GET, produces = APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public List<com.nordnet.vente.ws.entities.User> getUserByName(
-			@RequestParam(required = false, value = "name") final String name) throws ConverterException {
+			@RequestParam(required = false, value = "name") final String name)
+			throws ConverterException {
 		List<com.nordnet.vente.ws.entities.User> users = new ArrayList<com.nordnet.vente.ws.entities.User>();
 		for (User user : userService.getUserByName(name)) {
-			users.add(converter.convert(user, com.nordnet.vente.ws.entities.User.class));
+			users.add(converter.convert(user,
+					com.nordnet.vente.ws.entities.User.class));
 		}
 		return users;
 	}
@@ -77,7 +81,8 @@ public class UserController extends WadlController {
 	@ResponseBody
 	public List<com.nordnet.vente.ws.entities.User> getUserByPeriod(
 			@RequestParam(value = "startFrom", required = true) String startFrom,
-			@RequestParam(value = "endTo", required = false) String endTo) throws ConverterException {
+			@RequestParam(value = "endTo", required = false) String endTo)
+			throws ConverterException {
 		// Handle parameters
 		LocalDateTime startDate = LocalDateTime.parse(startFrom);
 		LocalDateTime endDate = LocalDateTime.now();
@@ -87,7 +92,8 @@ public class UserController extends WadlController {
 
 		List<com.nordnet.vente.ws.entities.User> users = new ArrayList<com.nordnet.vente.ws.entities.User>();
 		for (User user : userService.getUserByPeriod(startDate, endDate)) {
-			users.add(converter.convert(user, com.nordnet.vente.ws.entities.User.class));
+			users.add(converter.convert(user,
+					com.nordnet.vente.ws.entities.User.class));
 		}
 		return users;
 	}
@@ -95,30 +101,37 @@ public class UserController extends WadlController {
 	@RequestMapping(value = "/add", method = POST, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 	@ResponseBody
 	@ApiOperation(value = "Add new user", notes = "Add a new user, return user with id")
-	public com.nordnet.vente.ws.entities.User addUser(@RequestBody final UserInfo userInfo) throws ConverterException {
+	public com.nordnet.vente.ws.entities.User addUser(
+			@RequestBody final UserInfo userInfo) throws ConverterException {
 		// Handle input
-		User user = converter.convert(userInfo, com.nordnet.vente.domain.model.User.class);
+		User user = converter.convert(userInfo,
+				com.nordnet.vente.domain.model.User.class);
 
 		// Service and return
-		return converter.convert(userService.addUser(user), com.nordnet.vente.ws.entities.User.class);
+		return converter.convert(userService.addUser(user),
+				com.nordnet.vente.ws.entities.User.class);
 	}
 
 	@RequestMapping(value = "/update/byusername", method = POST, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 	@ResponseBody
 	@ApiOperation(value = "update use by username", notes = "update user by username")
-	public com.nordnet.vente.ws.entities.User updateUserByUserName(@RequestBody final UserInfo userInfo)
-			throws ConverterException, VenteException {
+	public com.nordnet.vente.ws.entities.User updateUserByUserName(
+			@RequestBody final UserInfo userInfo) throws ConverterException,
+			VenteException {
 		// Handle input
-		User user = converter.convert(userInfo, com.nordnet.vente.domain.model.User.class);
+		User user = converter.convert(userInfo,
+				com.nordnet.vente.domain.model.User.class);
 
 		// Service and return
-		return converter.convert(userService.updateUserByUserName(user), com.nordnet.vente.ws.entities.User.class);
+		return converter.convert(userService.updateUserByUserName(user),
+				com.nordnet.vente.ws.entities.User.class);
 	}
 
 	@RequestMapping(value = "/delete/{userid}", method = DELETE, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 	@ResponseBody
 	@ApiOperation(value = "delete user", notes = "Delete user by id, if true then deleting success")
-	public Boolean deleteUser(@PathVariable("userid") final Long userid) throws VenteException {
+	public Boolean deleteUser(@PathVariable("userid") final Long userid)
+			throws VenteException {
 		return userService.deleteUser(userid);
 	}
 }
--- vente.ws/src/main/java/com/nordnet/vente/ws/converter/UserConverter.java
+++ vente.ws/src/main/java/com/nordnet/vente/ws/converter/UserConverter.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package com.nordnet.vente.ws.converter;
 
 import org.dozer.CustomConverter;
